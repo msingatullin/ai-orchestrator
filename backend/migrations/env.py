6aa2c7b00cf3527fa8637dce72b0bfc6b7eda2f9
msingatullin <164233875+msingatullin@users.noreply.github.com>
2025-07-29 01:39:12 +0300

    Fix boolean default in raw_messages migration

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -5,10 +5,14 @@ from sqlalchemy import pool
 
 from alembic import context
 from backend.app.models import Base
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+database_url = os.getenv("DATABASE_URL")
+if database_url:
+    config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
